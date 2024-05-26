2b302c93accce9dcd4efb0b90836bc5239b4d17d
Akash <akashkumar200022@gmail.com>
2024-05-26 23:11:21 +0530

    my changes

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,16 +1,5 @@
-// import { createStore, applyMiddleware } from "redux";
-// import { thunk } from "redux-thunk";
-// import { EcomReducer } from "./EcomReducer";
-// const store = createStore(EcomReducer, applyMiddleware(thunk));
-// export { store };
 import { createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { EcomReducer } from "./EcomReducer";
-
-const store = createStore(
-  EcomReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
-
+const store = createStore(EcomReducer, applyMiddleware(thunk));
 export { store };
